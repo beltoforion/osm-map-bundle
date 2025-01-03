b4d46155b69b435b7e4dbdefb69280a055b2d121
Ingo Berg <2202567+beltoforion@users.noreply.github.com>
2025-01-03 08:04:21 +0100

    marker style update

--- src/FeatureStyles.ts
+++ src/FeatureStyles.ts
@@ -7,12 +7,10 @@ export interface FeatureStyle {
 export type FeatureStyleDictionary = { [key: string]: FeatureStyle };
 
 export class LineStyle implements FeatureStyle {
-    private caption: string;
     private color: string;
     private width: number;
 
-    constructor(caption: string, color: string, width: number) {
-        this.caption = caption;
+    constructor(color: string, width: number) {
         this.color = color;
         this.width = width;
     }
@@ -28,28 +26,32 @@ export class LineStyle implements FeatureStyle {
 }
 
 export class AnnotatedPointStyle implements FeatureStyle {
-    private caption: string;
-    private color: string;
+    private foreground: string;
+    private background: string;
+    private strokeWidth: number;
+    private radius: number;
 
-    constructor(caption: string, color: string) {
-        this.caption = caption;
-        this.color = color;
+    constructor(foreground: string, background: string = '#000', radius : number = 8, strokeWidth: number = 1) {
+        this.foreground = foreground;
+        this.background = background;
+        this.strokeWidth = strokeWidth;
+        this.radius = radius;
     }
 
     public getStyle(): Style {
         return new Style({
             image: new CircleStyle({
-                fill: new Fill({ color: this.color }),
-                radius: 8,
-                stroke: new Stroke({ color: '#ff0', width: 1 })
+                fill: new Fill({ color: this.foreground }),
+                radius: this.radius,
+                stroke: new Stroke({ color: this.background, width: this.strokeWidth })
             }),
             text: new Text({
-                text: this.caption,
+                text: '',
                 scale: 1.3,
                 offsetX: 0,
                 offsetY: 20,
-                fill: new Fill({ color: '#000000' }),
-                stroke: new Stroke({ color: this.color, width: 3.5 })
+                fill: new Fill({ color: this.background }),
+                stroke: new Stroke({ color: this.foreground, width: this.strokeWidth })
             })
         })
     }
